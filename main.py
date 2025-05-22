bb512fbf62e02f9991ac1912281201d7055de894
agrawalashwin <ashwin.agrawal@gmail.com>
2025-05-21 21:53:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import openai
 BASE_DIR      = os.path.dirname(os.path.abspath(__file__))
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 STATIC_DIR    = os.path.join(BASE_DIR, "static")
-
+ 
 app = Flask(
     __name__,
     template_folder=TEMPLATES_DIR,
